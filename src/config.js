63f74a065bf1ed511804d9b0d7c2df029117a5cd
Skyy34 <87014745+Skyy34@users.noreply.github.com>
2021-08-15 21:45:20 +0200

    cx
    xc

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODc2MzkxNjAzNzQ3NTU3NDI2.YRjZSA.L3Dy5BJIjHC2XDoNMsaqLhLGD7k", //token bota
+    token: "ODc2MzkxNjAzNzQ3NTU3NDI2.YRjZSA.cdLQJoi-K88tABkJWhmr3QM_gP0", //token bota
     dev: ["782609390607532043"], //id administratra
     settings: {
         prefix: "." //prefix bota
